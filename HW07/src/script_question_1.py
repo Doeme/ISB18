ec89adfd849cdcb9fe92db1557169936679af118
Jakob <jakob.wittmann@tum.de>
2018-02-25 14:03:49 +0000

    Switched to "slim_optimize()" and implemented continuously saving of results

--- HW07/src/script_question_1.py
+++ HW07/src/script_question_1.py
@@ -4,68 +4,98 @@ import os
 from os.path import join
 import itertools
 import pickle
+from pickle import UnpicklingError
 import sys
 import math
+import optlang.interface
 
 
-def calc_maxmin_of_exchange_reaction(model, gene_pairs, exchange_id, threshold):
+def calc_maxmin_of_exchange_reaction(model, gene_pairs, exchange_id, threshold, simulation_id):
 
     print("Generate max/min values of exchange reaction")
 
-    # list of results, each element consists of
-    # (0) maximum biomass growth
-    # (1) minimum flux of metabolite when biomass is fixed to maximum
-    # (2) maximum flux of metabolite when biomass is fixed to maximum
-    results = []
-    gene_pairs_len = len(gene_pairs)
-    idx = 0
-    hits = 0
-    sys.stdout.write('   Genes: %s/%s (%s%s), hits: %s\r' % 
-            (idx, gene_pairs_len, int(math.ceil(idx/gene_pairs_len*100)), '%', hits))
-    for gene_pair in gene_pairs:
-        with model:
-            #Knock out genes in current pair
-            for gene in gene_pair:
-                gene.knock_out()
-    
-            # Opitmize for biomass
-            sol = model.optimize()
-            opt_bio = sol.objective_value
+    filename = "results_question_1_{}.dat".format(simulation_id)
+    with open(filename, 'wb') as results_file:
+        print("   Write results to \"{}\"".format(filename))
+        pickle.dump(simulation_id, results_file)
 
-            if opt_bio > threshold:
-                hits += 1
-                # Set minimum value for biomass production
-                reaction = model.reactions.get_by_id('Ec_biomass_iJO1366_core_53p95M')
-                reaction.lower_bound = opt_bio
-
-                model.objective = exchange_id
-                # Optimize for maximum flux of exchange reaction
-                sol = model.optimize()
-                max_flux = sol.objective_value
-                # Optimize for minimum flux of exchange reaction
-                model.objective_direction = 'min'
-                sol = model.optimize()
-                min_flux = sol.objective_value
-
-                #gene_pair = [gene.id for gene in gene_pair]
-
-                results.append([gene_pair, opt_bio, min_flux, max_flux])
-
-            # Update progress
-            idx += 1
-            sys.stdout.flush()
-            sys.stdout.write('   Genes: %s/%s (%s%s), hits: %s\r' % 
+        # list of results, each element consists of
+        # (0) maximum biomass growth
+        # (1) minimum flux of metabolite when biomass is fixed to maximum
+        # (2) maximum flux of metabolite when biomass is fixed to maximum
+        results = []
+        gene_pairs_len = len(gene_pairs)
+        idx = 0
+        hits = 0
+        sys.stdout.write('   Genes: %s/%s (%s%s), hits: %s\r' % 
                 (idx, gene_pairs_len, int(math.ceil(idx/gene_pairs_len*100)), '%', hits))
+        for gene_pair in gene_pairs:
+            with model:
+                #Knock out genes in current pair
+                for gene in gene_pair:
+                    gene.knock_out()
+        
+                # Opitmize for biomass
+                opt_bio = model.slim_optimize()
+                opt_bio_status = model.solver.status
+    
+                # Proceed if optimization solver was successful
+                if opt_bio_status == optlang.interface.OPTIMAL:
+                    if opt_bio > threshold:
+                        hits += 1
+                        # Set minimum value for biomass production
+                        reaction = model.reactions.get_by_id('Ec_biomass_iJO1366_core_53p95M')
+                        reaction.lower_bound = opt_bio
+        
+                        model.objective = exchange_id
+                        # Optimize for maximum flux of exchange reaction
+                        max_flux = model.slim_optimize()
+                        max_flux_status = model.solver.status
+                        # Optimize for minimum flux of exchange reaction
+                        model.objective_direction = 'min'
+                        min_flux = model.slim_optimize()
+                        min_flux_status = model.solver.status
+                    else:
+                        opt_bio_status = 'threshold'
+                        max_flux = 'None'
+                        max_flux_status = 'None'
+                        min_flux = 'None'
+                        min_flux_status = 'None'
+                else:
+                    max_flux = 'None'
+                    max_flux_status = 'None'
+                    min_flux = 'None'
+                    min_flux_status = 'None'
 
+                # Save results to file and flush all buffers
+                pickle.dump([
+                    gene_pair, 
+                    opt_bio_status, 
+                    opt_bio, 
+                    min_flux_status, 
+                    min_flux, 
+                    max_flux_status, 
+                    max_flux], results_file)
+                results_file.flush()
+                os.fsync(results_file.fileno())
+    
+                # Update progress
+                idx += 1
+                sys.stdout.flush()
+                sys.stdout.write('   Genes: %s/%s (%s%s), hits: %s\r' % 
+                    (idx, gene_pairs_len, int(math.ceil(idx/gene_pairs_len*100)), '%', hits))
+
+    
 
     sys.stdout.write('\n')
     return results
 
 
 
+path_to_models = "./../"
 
 print("Load matlab model...")
-model = cobra.io.load_matlab_model(join("Model_iJO1366.mat"))
+model = cobra.io.load_matlab_model(join(path_to_models, "Model_iJO1366.mat"))
 
 # Create list of knock out gene pairs
 #gene_ids = [ gene.id for gene in model.genes ]
@@ -76,7 +106,7 @@ model = cobra.io.load_matlab_model(join("Model_iJO1366.mat"))
 gene_pairs = list(itertools.combinations(model.genes, 2))
 
 # Shorten the list a bit during debugging
-#gene_pairs = gene_pairs[:100]
+gene_pairs = gene_pairs[:100]
 
 # Exchange reaction     Metabolite name
 # =====================================
@@ -110,14 +140,7 @@ exchanges['mutial_lactate'] = mutual_lactate
 
 for key, exc in exchanges.items():
     print("Load exchange reaction \"{}\"".format(key))
-    results = calc_maxmin_of_exchange_reaction(model, gene_pairs, exc, 0)
-
-    filename = "results_question_1_{}.dat".format(key)
-    with open(filename, 'wb') as tmp_file:
-        pickle.dump(key, tmp_file)
-        pickle.dump(results, tmp_file)
-
-    print("   Saved results in file \"{}\"".format(filename))
+    calc_maxmin_of_exchange_reaction(model, gene_pairs, exc, 0, key)
 
 print("END")
 
