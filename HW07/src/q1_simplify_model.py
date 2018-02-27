4d702a86fe3183b9cd6d9e585b09390d6f89fa72
Jakob <jakob.wittmann@tum.de>
2018-02-27 15:34:48 +0000

    Fixed FVA call to ignore previous optimization solution

--- HW07/src/q1_simplify_model.py
+++ HW07/src/q1_simplify_model.py
@@ -8,6 +8,9 @@ import math
 def pandas_to_list(pandas):
     return [ list(t) for t in pandas.itertuples() ]
 
+def pandas_to_dict(pandas):
+    return { t[0]: { 'max': t[1], 'min': t[2] } for t in pandas.itertuples() }
+
 def compare_fva_results_with():
     path_to_models = './../'
     
@@ -47,7 +50,7 @@ def compare_fva_results_with():
 
 def find_unused_reactions(model, threshold=1e-9):
     with model:
-        presults = flux_variability_analysis(model, model.reactions)
+        presults = flux_variability_analysis(model, model.reactions, fraction_of_optimum=0)
         results = pandas_to_list(presults)
 
         reaction_ids = []
@@ -56,7 +59,7 @@ def find_unused_reactions(model, threshold=1e-9):
                 reaction_ids.append(res[0])
                 #print("max: {}, min: {}".format(res[1], res[2]))
 
-    return reaction_ids, results
+    return reaction_ids, presults
 
 def simplify_model_with_fva(model, threshold=1e-9):
 
@@ -66,17 +69,89 @@ def simplify_model_with_fva(model, threshold=1e-9):
 
     return reactions, results
 
-#path_to_models = './../'
-#
-#print("Load model...")
-#model = cobra.io.load_json_model(join(path_to_models, 'ecolicore.json'))
-#
-## Set input fluxes of o2 and gcl to maximum
-#EX_o2 = model.reactions.get_by_id('EX_o2(e)')
-#EX_o2.lower_bound = -1000
-#EX_glc = model.reactions.get_by_id('EX_glc(e)')
-#EX_glc.lower_bound = -1000
-#
+def plot_fva_results_noise(model, reaction_ids, repetitions=10):
+    
+    results = []
+    for i in range(0, repetitions):
+        with model:
+            reactions = [ model.reactions.get_by_id(ex_id) for ex_id in reaction_ids ]
+            results.append(pandas_to_dict(flux_variability_analysis(model, reactions)))
+
+    values = { r_id: {'max': [ res[r_id]['max'] for res in results ], 'min': [ res[r_id]['min'] for res in results ] } for r_id in reaction_ids }
+
+    plt.subplot(2,2,1)
+    plt.hist(values['EX_succ(e)']['max'])
+    plt.title('EX_succ(e) maxima')
+
+    plt.subplot(2,2,3)
+    plt.hist(values['EX_succ(e)']['min'])
+    plt.title('EX_succ(e) minima')
+
+    plt.subplot(2,2,2)
+    plt.hist(values['EX_etoh(e)']['max'])
+    plt.title('EX_etoh(e) maxima')
+
+    plt.subplot(2,2,4)
+    plt.hist(values['EX_etoh(e)']['min'])
+    plt.title('EX_etoh(e) minima')
+    
+    plt.show()
+
+if __name__ == "__main__":
+
+
+    path_to_models = './../'
+
+    print("Load model...")
+    model = cobra.io.load_json_model(join(path_to_models, 'ecolicore.json'))
+
+    # Set input fluxes of o2 and gcl to maximum
+    EX_o2 = model.reactions.get_by_id('EX_o2(e)')
+    EX_o2.lower_bound = -1000
+    EX_glc = model.reactions.get_by_id('EX_glc(e)')
+    EX_glc.lower_bound = -1000
+
+    exchanges = {
+        'acetate': 'EX_ac(e)',
+        'd-lactate': 'EX_lac-D(e)',
+        'l-lactate': 'EX_lac-L(e)',
+        'succinate': 'EX_succ(e)',
+        'ethanol': 'EX_etoh(e)'}
+
+#    plot_fva_results_noise(model, list(exchanges.values()) ) 
+
+    reaction_ids, results = find_unused_reactions(model, threshold=1e-11)
+
+    d_results = pandas_to_dict(results)
+
+    print("Results of exchanges:")
+    print("Exchange\tmax\tmin")
+    print("==================")
+    for key, ex_id in exchanges.items():
+        print("{}\t{}\t{}".format(ex_id, d_results[ex_id]['max'], d_results[ex_id]['min']))
+
+    del_exchanges = []
+    for key, ex in exchanges.items():
+        try:
+            reaction_ids.index(ex)
+            del_exchanges.append(ex)
+        except ValueError:
+            pass
+
+#    print("Exchange ID\tStatus")
+#    print("===================")
+#    for idx in range(0, len(exchanges)):
+#        print("{}\t{}".format(exchanges
+
+    print("Deleted reactions:")
+    print(del_exchanges)
+
+
+
+
+
+
+
 #with model:
 #    results = pandas_to_list(flux_variability_analysis(model, model.reactions))
 #
