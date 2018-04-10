c30670aa2b990e10987e953f1ef64441bb09d9bf
Dominik Schmidt <das1993@hotmail.com>
2018-04-10 23:39:45 +0000

    Work on the code

--- final_project/src/dmmm.py
+++ final_project/src/dmmm.py
@@ -9,59 +9,105 @@ class Organism:
 	population=1.0;
 	alpha=1.0;
 	model=None;
+	exchange_filter=None;
 	initial_lb={};
+
 	def __init__(self, model, population, mortality):
 		self.mortality=mortality;
 		self.population=population;
 		self.model=model;
-		self.initial_lb={r.id:r.lower_bound for r in model.exchanges};
+		self.initial_lb={r.id:r.lower_bound for r in self.exchanges()};
+		retval=np.zeros(len(model.metabolites));
 
 	def density_to_lower_bound(self, densities, id):
 		s=densities[id];
-		vm=abs(self.initial_lb[id]);
-		k=1;
-		return -vm*s/(k+s);
+		vm=self.initial_lb[id];	#This is usually negative
+		k=0.1;
+		return vm*s/(k+s);		#Therefore, this is usually too
 
 	def adapt_boundaries(self, densities):
-		for id,r in [(r.id,r) for r in self.model.exchanges]:
+		for id,r in [(r.id,r) for r in self.exchanges()]:
 			r.lower_bound=self.density_to_lower_bound(densities,id);
+	
+	def exchanges(self):
+		if self.exchange_filter:
+			return filter(self.exchange_filter,self.model.exchanges);
+		return self.model.exchanges;
 
-	def fba(self,densities):
+	def fba(self,medium,densities):
 		self.adapt_boundaries(densities);
 		sol=self.model.optimize();
 		if sol.status!='optimal':
-			return (0,{});
+			return (0,0);
 		assert(sol.objective_value >= 0);
 
 		return (
 			sol.objective_value*self.alpha-self.mortality,
-			dict(sol[[r.id for r in self.model.exchanges]])
+			medium.dict_to_list(sol[[r.id for r in self.exchanges()]])
 		);
 
 class Medium:
 	metabolites={};
+	lut=[];
 
 	def __init__(self, organisms):
-		models=map(lambda x: x.model,organisms);
-		ids=map(lambda x: {r.id for r in x.exchanges},models);
+		ids=map(lambda x: {r.id for r in x.exchanges()},organisms);
 		medium_ids=functools.reduce(lambda x,y: x.union(y), ids);
 		self.metabolites=dict.fromkeys(medium_ids, 0.0);
+		self.lut=[id for id in self.metabolites.keys()];
 
 	def densities(self):
 		total=sum(self.metabolites);
 		return {k:v/total for k,v in self.metabolites.items()};
+		
+	def list_to_dict(self, l):
+		return {k:v for k,v in zip(self.lut,l)};
+		
+	def list_to_densities(self, l, total):
+		return {k:v/total for k,v in zip(self.lut,l)};
+		
+	def dict_to_list(self, d):
+		return [d.get(k,0) for k in self.lut];
+
+class Result:
+	dmmm=None;
+	state=[];
+	times=[];
+	
+	def __init__(self,dmmm):
+		self.dmmm=dmmm;
+	
+	def append(self,t,y):
+		self.times.append(t);
+		self.state.append(y);
+	
+	def get_population(self, idx):
+		idx=self.dmmm.medium.lut.index(name);
+		return {t:self.dmmm.split_state(self.state[i])[1][idx] for i,t in enumerate(self.times)};
+	
+	def get_metabolite(self, name):
+		idx=self.dmmm.medium.lut.index(name);
+		return {t:self.dmmm.split_state(self.state[i])[1][idx] for i,t in enumerate(self.times)};
+	
+	def get_metabolites_at_step(self, step):
+		return self.dmmm.medium.list_to_dict(self.dmmm.split_state(self.state[step])[1]);
 
 class DMMM:
 	organisms=[];
 	medium=None;
+	ret=None;
+	buf=None;
+	densities=None;
 
 	def __init__(self, organisms):
 		self.organisms=organisms;
 		self.medium=Medium(organisms);
+		self.ret=np.zeros(self.state_length());
+		self.buf=np.zeros(self.state_length());
+		self.densities={k:0 for k in self.medium.lut};
 
-	def adapt_boundaries(self):
-		for organism in self.organisms:
-			organism.adapt_boundaries(self.medium);
+	def state_length(self):
+		return len(self.medium.metabolites)+len(self.organisms);
 
 	def simulate(self, delta_t):
 		self.adapt_boundaries();
@@ -69,26 +115,38 @@ class DMMM:
 			dM=organism.simulate(delta_t);
 			for k,v in dM.items():
 				self.medium.metabolites[k]+=v;
-
+	
+	def split_state(self,s):
+		return (s[0:len(self.organisms)],s[len(self.organisms):]);
+	
 	def func(self, t, y):
-		c=Counter({ k:0 for k in self.medium.metabolites.keys()});
-		populations=y[0:len(self.organisms)];
-		metabolites=y[len(self.organisms):];
+		self.buf[:]=[max(0,v) for v in y];
+		(populations,metabolites)=self.split_state(self.buf);
+		(growth,fluxes)=self.split_state(self.ret);
+		self.ret[:]=0;
+		
+		#print("State");
+		#print(populations);
+		#print(self.medium.list_to_dict(metabolites));
 		total=sum(metabolites);
-		densities={k:metabolites[idx]/total for idx,k in enumerate(self.medium.metabolites.keys())};
-		ret=np.zeros(len(y));
+		self.densities.update(self.medium.list_to_densities(metabolites,total));
+		
 		for idx,o in enumerate(self.organisms):
-			(bio,met)=o.fba(densities);
-			ret[idx]=bio;
-			c=c+Counter({k:v*y[idx] for k,v in met.items()});
-		ret[len(self.organisms):]=[c[id] for id in self.medium.metabolites.keys()];
-		return ret;
+			if populations[idx]>0: #No need to calculate the fba on dead populations
+				(bio,met)=o.fba(self.medium, self.densities);
+				growth[idx]=bio*populations[idx];
+				fluxes[:]=fluxes*populations[idx]+met;
+		#print("Derivative");
+		#print(growth)
+		#print(self.medium.list_to_dict(fluxes));
+		#assert(False);
+		return self.ret;
 
 	def get_ode(self):
 		ode=scipy.integrate.ode(self.func);
 		l=[];
 		l.extend([v.population for v in self.organisms]);
-		l.extend([v for v in self.medium.metabolites.values()]);
+		l.extend([self.medium.metabolites[id] for id in self.medium.lut]);
 		ode.set_initial_value(l,0);
 		#ode.set_f_params(self);
 		ode.set_integrator('dopri5')
@@ -96,7 +154,8 @@ class DMMM:
 
 	def simulate(self, t):
 		ode=self.get_ode();
-		l=[];
-		ode.set_solout(lambda t,y: print(t,y)); #l.append((t,y)));
+		r=Result(self);
+		ode.set_solout(lambda t,y: r.append(t,y.copy()));
 		ode.integrate(t);
-		return l;
+		return r;
+
--- final_project/src/test.py
+++ final_project/src/test.py
@@ -1,22 +1,30 @@
 import dmmm;
 import cobra;
 
-l=cobra.io.read_sbml_model("../Models/lactobacillus.xml")
-ll=dmmm.Organism(l,1000,0)
+l=cobra.io.read_sbml_model("../models/lactobacillus.sbml")
+ll=dmmm.Organism(l,1,0.0)
+ll.exchange_filter=lambda x: x.id!="added_biomass_sink";
 
-y=cobra.io.read_sbml_model("../Models/yeast.xml")
-yy=dmmm.Organism(y,100000,0)
+y=cobra.io.read_sbml_model("../models/yeast.sbml")
+yy=dmmm.Organism(y,1000,0.0)
 
 co=[ll,yy]
 
 d=dmmm.DMMM(co)
 
-d.medium.metabolites["EX_glc_e_"]=1000
-d.medium.metabolites["EX_o2_e_"]=50
 
 for k,v in d.medium.metabolites.items():
-	d.medium.metabolites[k]=v+1;
+	d.medium.metabolites[k]=v+100;
+
+vol=50;
+mol_vol=55346;
+perc_glc=0.10;
+
+d.medium.metabolites["EX_h2o_e_"]=(1-perc_glc)*vol*mol_vol*1000;
+d.medium.metabolites["EX_glc_e_"]=(perc_glc)*vol*mol_vol*1000;
+d.medium.metabolites["EX_o2_e_"]=(perc_glc)*vol*mol_vol*1000*10;
 
 
-l=d.simulate(0.1);
-print(l);
+l=d.simulate(14*24);
+
+import IPython; IPython.embed()
