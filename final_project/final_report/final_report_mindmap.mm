0369d356c5c81d61045e716db9c8c4bc295409b1
Jakob <jakob.wittmann@tum.de>
2018-04-17 12:13:45 +0000

    added new sources about metabolite inhibition in S. cerevisiae and L. plantarum co-cultures

--- final_project/final_report/final_report_mindmap.mm
+++ final_project/final_report/final_report_mindmap.mm
@@ -6,6 +6,23 @@
 <node CREATED="1523622195172" ID="ID_190233608" MODIFIED="1523637563952" TEXT="in alcohol fermentation in food industry (wine, beer)"/>
 <node CREATED="1523625370979" ID="ID_1714801516" MODIFIED="1523625376778" TEXT="take beer brewing as an example"/>
 <node CREATED="1523622236244" ID="ID_919210626" MODIFIED="1523622252152" TEXT="high quality requirements"/>
+<node CREATED="1523897837073" ID="ID_1093146388" MODIFIED="1523898188318" TEXT="\cite{narendranath_effects_1997}">
+<node CREATED="1523898227308" ID="ID_105287263" MODIFIED="1523898230848" TEXT="&quot;Bacterial contamination is a major cause of reduction in ethanol yield during fermentation of starch-based feedstocks by Saccharomyces cerevisiae.&quot;"/>
+<node CREATED="1523898290696" ID="ID_17420011" MODIFIED="1523898293254" TEXT="&quot;Among the bacterial contami- nants encountered, the lactic acid bacteria are the most trou- blesome because of their tolerance to high temperature and low pH and their ability to grow rapidly.&quot;"/>
+<node CREATED="1523898320574" ID="ID_323259808" MODIFIED="1523898324220" TEXT="&quot;The genus Lactoba- cillus is of major concern to distilleries and fuel alcohol plants, whereas in breweries, the genus Pediococcus is a persistent contaminant which often produces off-flavors&quot;"/>
+<node CREATED="1523898377843" ID="ID_1035011255" MODIFIED="1523898380216" TEXT="&quot;Contam- inants can arise from tankage, transfer lines, heat exchangers, raw materials, active dry yeast, poorly stored backset (recycled thin stillage), or yeast slurry used as the inoculum (23).&quot;"/>
+<node CREATED="1523898431981" ID="ID_1307672818" MODIFIED="1523898434251" TEXT="&quot;Micro- bial numbers can be significantly reduced by cleaning and san- itizing the equipment&quot;"/>
+<node CREATED="1523898454858" ID="ID_589471449" MODIFIED="1523898457577" TEXT="&quot;In spite of these precautions, bacterial contamination still persists in many ethanol produc- tion plants&quot;"/>
+<node CREATED="1523898514519" ID="ID_942061565" MODIFIED="1523898517171" TEXT="&quot; When up to 109 CFU of bacteria/ml was present in mash, approximately 3.8 to 7.6% reductions in ethanol concentration occurred depending on the strain.&quot;"/>
+<node CREATED="1523902833022" ID="ID_1519971337" MODIFIED="1523902836633" TEXT="&quot;In large plants with outputs of 400 million to 1,100 million liters of ethanol per year, such a decrease would reduce income by $1 million to 3 million annually&quot;"/>
+</node>
+<node CREATED="1523901848986" ID="ID_148143504" MODIFIED="1523901855587" TEXT="\cite{makanjuola_effects_1992}">
+<node CREATED="1523901858913" ID="ID_1081118688" MODIFIED="1523901977336" TEXT="industrial ethanol production must be very efficient as the proifit margin is not very high"/>
+<node CREATED="1523901978089" ID="ID_1020959297" MODIFIED="1523901998635" TEXT="decrease of ethanol production leads to lower profit"/>
+<node CREATED="1523902302865" ID="ID_1347294106" MODIFIED="1523902305810" TEXT="&quot;studies on the direct effects of contamination are not easily carried out. The differences between and within batches of raw materials pro- duce differences in chemical composition and measured fer- mentation parameters. Raw materials themselves may harbor contaminating bacteria which compete with yeasts for growth- promoting nutrients.&quot;">
+<node CREATED="1523902316504" ID="ID_1947415530" MODIFIED="1523902388427" TEXT="so, simulations with varying starting conditions would help development and to find possible reasons for changing bacteria growth"/>
+</node>
+</node>
 <node CREATED="1523622252453" ID="ID_919038601" MODIFIED="1523622271127" TEXT="many restrictions how to influence the fermentation">
 <node CREATED="1523638355811" ID="ID_998580945" MODIFIED="1523638389540" TEXT="\cite{biergesetz_1993}"/>
 <node CREATED="1523639972602" ID="ID_1401935815" MODIFIED="1523639983493" TEXT="\cite{weingesetz_1994}"/>
@@ -69,6 +86,18 @@
 <node CREATED="1523627352961" ID="ID_1500317686" MODIFIED="1523627362944" TEXT="Simulation Setup">
 <node CREATED="1523627365600" ID="ID_1750906473" MODIFIED="1523627369495" TEXT="used GEMs"/>
 <node CREATED="1523627370925" ID="ID_405200565" MODIFIED="1523627380852" TEXT="constants"/>
+<node CREATED="1523966723693" ID="ID_1320421240" MODIFIED="1523966773405" TEXT="maybe use an alcohol- and acid inhibition constant in the Michaelis-Menten formula?">
+<icon BUILTIN="help"/>
+<node CREATED="1523966793841" ID="ID_1137990873" MODIFIED="1523966797291" TEXT="examles:">
+<node CREATED="1523622175744" ID="ID_1244953746" MODIFIED="1523820068743" TEXT="\cite{pizarro_coupling_2007}">
+<node CREATED="1523967060492" ID="ID_267471306" MODIFIED="1523967086576" TEXT="formula 1/(1+E/K_e)"/>
+</node>
+<node CREATED="1523967050732" ID="ID_26636587" MODIFIED="1523967055278" TEXT="\cite{hanly_dynamic_2011}">
+<node CREATED="1523967060492" ID="ID_670981976" MODIFIED="1523967086576" TEXT="formula 1/(1+E/K_e)"/>
+<node CREATED="1523967090257" ID="ID_217319381" MODIFIED="1523967132738" TEXT="constant of S. cerevisiae for ethanol inhibition"/>
+</node>
+</node>
+</node>
 </node>
 </node>
 <node CREATED="1523621487455" ID="ID_14969992" MODIFIED="1523621491036" POSITION="right" TEXT="Results"/>
@@ -94,6 +123,58 @@
 <node CREATED="1523628240691" ID="ID_1926795350" MODIFIED="1523628260988" TEXT="input flux of oxygen via solution surface"/>
 </node>
 <node CREATED="1523628264914" ID="ID_1984953785" MODIFIED="1523628358184" TEXT="verwertung der bestandteile von abgestorbenen bakterien in der L&#xf6;sung"/>
+</node>
+</node>
+</node>
+<node CREATED="1523902496885" ID="ID_1045395905" MODIFIED="1523902503703" POSITION="right" TEXT="References">
+<node CREATED="1523902506662" ID="ID_752597977" MODIFIED="1523902506662" TEXT="\cite{narendranath_effects_1997}">
+<node CREATED="1523902509570" ID="ID_124116858" MODIFIED="1523902528357" TEXT="yeast - lactobacillus model">
+<node CREATED="1523902772862" ID="ID_800604751" MODIFIED="1523902792067" TEXT="Lactic acid inhibits yeast growth and metabolism">
+<node CREATED="1523902595263" ID="ID_877282578" MODIFIED="1523902797722" TEXT="&quot; lactic acid, inhibits yeast growth and metabo- lism (18) and was the major cause of the decrease in ethanol yield in this study&quot;"/>
+<node CREATED="1523902621263" ID="ID_571177653" MODIFIED="1523902797716" TEXT="&quot;The two acids together [note: lactic acid and acetic acid] have been shown to have a synergistic and negative effect on yeast growth and metabolism (20)&quot;"/>
+<node CREATED="1523902692696" ID="ID_1329537894" MODIFIED="1523902797710" TEXT="&quot;However, data for the action of acetic acid and lactic acid on yeasts show growth inhibition different from that predicted on the basis of dissociation con- stants, indicating that these acids may not act in the same manner on yeast cells (18, 20). &quot;"/>
+</node>
+<node CREATED="1523902715262" ID="ID_70434494" MODIFIED="1523902726948" TEXT="Biotin as mutual nutrient">
+<node CREATED="1523902729602" ID="ID_409124602" MODIFIED="1523902732399" TEXT="&quot;Biotin and vitamin B12 are required by a few strains (15, 16). Biotin is also an essential growth factor for S. cerevisiae (16).&quot;"/>
+<node CREATED="1523902747412" ID="ID_1627914295" MODIFIED="1523902767984" TEXT="&quot; Since the growth rates of the selected lactobacilli are higher than the growth rates of S. cerevisiae and lactic acid bacteria found in breweries and food products, removal of essential growth factors could result in reductions in the yeast growth rate and catalytic activity. This would reduce the final ethanol yield. &quot;"/>
+</node>
+</node>
+<node CREATED="1523902533759" ID="ID_1625862872" MODIFIED="1523902539645" TEXT="simulation results">
+<node CREATED="1523903208421" ID="ID_1069066214" MODIFIED="1523903216923" TEXT="bacteria densities over time">
+<node CREATED="1523902925466" ID="ID_828419721" MODIFIED="1523903219220" TEXT="Figure 2 (A)">
+<node CREATED="1523902933458" ID="ID_224543738" MODIFIED="1523902979665" TEXT="Growth of L. plantarum over time in co-culture with yeast"/>
+<node CREATED="1523902998942" ID="ID_163725371" MODIFIED="1523903119842" TEXT="yeast cells at approximately 10&#x2076; CFU/ml."/>
+</node>
+<node CREATED="1523903043577" ID="ID_1314103257" MODIFIED="1523903219217" TEXT="Figure 4">
+<node CREATED="1523903048245" ID="ID_141993394" MODIFIED="1523903063556" TEXT="Growth of yeast over time in co-culture with L. plantarum"/>
+<node CREATED="1523903078636" ID="ID_319005254" MODIFIED="1523903111718" TEXT="use here same init. yeast cell density as in Figure 2: 10&#x2076; CFU/ml"/>
+</node>
+</node>
+<node CREATED="1523903225967" ID="ID_844703647" MODIFIED="1523903235207" TEXT="final metabolte densities">
+<node CREATED="1523903453551" ID="ID_1559081085" MODIFIED="1523903456578" TEXT="Lactic Acid">
+<node CREATED="1523903248652" ID="ID_1126681698" MODIFIED="1523903457848" TEXT="Figure 3">
+<node CREATED="1523903253239" ID="ID_1651097993" MODIFIED="1523903383811" TEXT="final percentage of lactic acid in mash in w/v"/>
+<node CREATED="1523902998942" ID="ID_273178701" MODIFIED="1523903119842" TEXT="yeast cells at approximately 10&#x2076; CFU/ml."/>
+<node CREATED="1523903406515" ID="ID_995541880" MODIFIED="1523903425223" TEXT="I think it was measured after 72h"/>
+</node>
+</node>
+<node CREATED="1523903447975" ID="ID_1192851966" MODIFIED="1523903451706" TEXT="Ethanol">
+<node CREATED="1523903469928" ID="ID_1532772771" MODIFIED="1523903475261" TEXT="Table 1">
+<node CREATED="1523903477264" ID="ID_487961605" MODIFIED="1523903523463" TEXT="alcohol concentration in % vol/vol after 72h"/>
+<node CREATED="1523902998942" ID="ID_582179728" MODIFIED="1523903571742" TEXT="yeast cells at approximately 10&#x2076; CFU/ml."/>
+</node>
+</node>
+</node>
+<node CREATED="1523903617910" ID="ID_1460819006" MODIFIED="1523903623639" TEXT="possible verification">
+<node CREATED="1523903625262" ID="ID_1073908142" MODIFIED="1523903748058" TEXT="show linear dependency in Figure 3 (L. plantarum to lactic acid concentration)"/>
+<node CREATED="1523903659668" ID="ID_1601477934" MODIFIED="1523903719771" TEXT="show linear dependency in Figure 5 (L. plantarum concentration to alcohol production decrease)"/>
+</node>
+<node CREATED="1523903772445" ID="ID_1060913936" MODIFIED="1523903777310" TEXT="initial conditions">
+<node CREATED="1523902998942" ID="ID_813871406" MODIFIED="1523903119842" TEXT="yeast cells at approximately 10&#x2076; CFU/ml."/>
+<node CREATED="1523903802021" ID="ID_978764375" MODIFIED="1523903852535" TEXT="L. plantarum cells at approximately 10&#x2075;, 10&#x2076;, 10&#x2077;, 10&#x2078;, and 10&#x2079; CFU/ml"/>
+<node CREATED="1523903881591" ID="ID_248831135" MODIFIED="1523903892447" TEXT="Glucose concentration: 22 to 24&#xb0; Plato"/>
+<node CREATED="1523903916968" ID="ID_939832867" MODIFIED="1523903925763" TEXT="temperature: 30&#xb0;C"/>
+</node>
 </node>
 </node>
 </node>
